cb352dd8114b8a4193af8e9e5ad39d333cfbde3d
kikulabo <kikuchi@x-tech5.co.jp>
2022-02-21 15:48:07 +0900

    update: 変数化およびリソース参照するように変更

--- project.tf
+++ project.tf
@@ -1,5 +1,26 @@
-resource "sentry_project" "kikulabo-project" {
+locals {
+  project_map = [
+    {
+      project_name = "kikulabo-pj-1",
+      team_name    = sentry_team.kikulabo["kikulabo-team-1"].slug,
+      platform     = "go",
+      resolve_age  = 0
+    },
+    {
+      project_name = "kikulabo-pj-2",
+      team_name    = sentry_team.kikulabo["kikulabo-team-2"].slug,
+      platform     = "python",
+      resolve_age  = 0
+    },
+  ]
+}
+
+resource "sentry_project" "kikulabo" {
   organization = data.sentry_organization.org_kikulabo.id
-  team         = sentry_team.kikulabo-team.slug
-  name         = "kikulabo-project-new"
+  for_each     = { for i in local.project_map : i.project_name => i }
+  team         = each.value.team_name
+  name         = each.value.project_name
+  slug         = each.value.project_name
+  platform     = each.value.platform
+  resolve_age  = each.value.resolve_age
 }
--- team.tf
+++ team.tf
@@ -1,4 +1,13 @@
-resource "sentry_team" "kikulabo-team" {
+locals {
+  team_list = [
+    "kikulabo-team-1",
+    "kikulabo-team-2"
+  ]
+}
+
+resource "sentry_team" "kikulabo" {
   organization = data.sentry_organization.org_kikulabo.id
-  name         = "kikulabo-new"
+  for_each     = toset(local.team_list)
+  name         = each.key
+  slug         = each.key
 }
